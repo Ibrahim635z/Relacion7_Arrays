acad5f504b913faa3776aa18833286c5362ebb6c
Ibrahim635z <imohahm635z@g.educaand.es>
2024-11-14 14:07:01 +0100

    ej9

--- Ejercicio8.java
+++ Ejercicio8.java
@@ -1,5 +1,44 @@
 package Relacion7_Arrays;
 
+import java.util.Scanner;
+
 public class Ejercicio8 {
+    public static void main(String[] args) {
+         Scanner entrada= new Scanner(System.in);
+         int generador[]=new int [100];
+         int valor1=0,valor2=0;
+         
+        //GENERAMOS LOS VALORES
+         for (int i = 0; i < generador.length; i++) {
+            generador[i]= (int) (Math.random()*20);
+         }
+
+         //LOS MOSTRAMOS POR PANTALLA
+
+         for (int i = 0; i < generador.length; i++) {
+            System.out.print(" " +generador[i]);   
+         }
+
+         //PEDIMOS LOS VALORES PARA SUSTITUIRLOS
+
+         System.out.println("");
+         System.out.println("Ingrese el primer valor");
+         valor1=entrada.nextInt();
+         System.out.println("Ingrese el segundo valor");
+         valor2=entrada.nextInt();
+
+        // SUSTITUIMOS LOS VALORES
+         for (int i = 0; i < generador.length; i++) {
+            if(generador[i]==valor1){
+                generador[i]=valor2;
+            }
+         }
+         //MOSTRAMOS POR PANTALLA LA MODIFICACION
+
+         for (int i = 0; i < generador.length; i++) {
+            System.out.print(" " +generador[i]);   
+         }
+         entrada.close();
+    }
     
 }
--- Ejercicio9.java
+++ Ejercicio9.java
@@ -1,5 +1,34 @@
 package Relacion7_Arrays;
 
+import java.util.Scanner;
+
 public class Ejercicio9 {
+    public static void main(String[] args) {
+         Scanner entrada= new Scanner(System.in);
+         int generador[]=new int [20];
+         int pos_par=0,pos_impar=generador.length-1;
+
+        //colocar los pares al principio y los impares al final desde atras
+        
+        for (int i = 0; i < generador.length; i++) {
+            int num=(int) (Math.random()*100);
+            if(num%2==0){
+                generador[pos_par]=num;
+                pos_par++;
+            }else{
+                generador[pos_impar]=num;
+                pos_impar--;
+                }
+            }
+
+                //mostrar por pantalla
+
+        for (int j = 0; j < generador.length; j++) {
+                System.out.print(" " +generador[j]);    
+        }
+
+
+         entrada.close();
+    }
     
 }
