4f098dfd0d79923ee7704c50dac971252f7c0221
Ibrahim635z <imohahm635z@g.educaand.es>
2024-11-20 00:05:02 +0100

    Ej12

--- Ejercicio12.java
+++ Ejercicio12.java
@@ -6,7 +6,8 @@ public class Ejercicio12 {
     public static void main(String[] args) {
         Scanner entrada=new Scanner(System.in);
         int generador[][]=new int[4][5];
-        
+
+        //Pedir los valores del array bidimensional
         for (int fila = 0; fila < generador.length; fila++) {
             for (int columna = 0; columna < generador[fila].length; columna++) {
                 System.out.print("Ingrese el valor ["+fila+"]"+"["+columna+"]: ");
@@ -16,21 +17,40 @@ public class Ejercicio12 {
         
         int suma_fila=0;
 
+        //mostrarlo por pantalla con la suma de sus filas
 
         for (int fila = 0; fila < generador.length; fila++) {
             for (int columna = 0; columna < generador[fila].length; columna++) {
               
-                System.out.print("    " +generador[fila][columna]);
+                System.out.print("            " +generador[fila][columna]);
                 suma_fila+=generador[fila][columna];
                 
             }
            
-            System.out.println("   Suma fila"+fila+":"+suma_fila);
+            System.out.println("         Suma fila"+fila+":"+suma_fila);
             suma_fila=0;
             System.out.println();
         }
         
         System.out.println();
+
+        int suma_colum[]=new int[5];
+        
+
+        for (int columna = 0; columna < 5; columna++) {
+            for (int fila = 0; fila < generador.length; fila++) {
+                
+                suma_colum[columna]+=generador[fila][columna];
+                
+            }
+           
+          
+           System.out.print("Sum Colum"+columna+": " +suma_colum[columna]+ " |");
+           
+        }        
+
+
+
      
         entrada.close();
     }
