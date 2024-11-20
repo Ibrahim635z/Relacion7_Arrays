083a0f2c11167d735a5b3ef111380b6891ab3a97
Ibrahim635z <imohahm635z@g.educaand.es>
2024-11-20 14:23:47 +0100

    casi_ej13

--- Ejercicio13.java
+++ Ejercicio13.java
@@ -6,10 +6,20 @@ public class Ejercicio13 {
     public static void main(String[] args) {
         Scanner entrada=new Scanner(System.in);
         String paises[]=new String[] {"España","Rusia","Japon","Australia"};
-        
+        int array[][]=new int[4][10];
 
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j]=(int) (Math.random() *70+140);
+            }
+        }
 
-
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j]+"     ");
+            }
+            System.out.println();
+        }
         entrada.close();
     }
     
